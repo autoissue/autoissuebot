3a082b6b374cd9225697f7d8f7e4d3aae4995f92
Vat Raghavan <machinshin@gmail.com>
2020-06-15 16:27:03 -0700

    show issues

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const context = github.context;
+const issueParser = require('issue-parser');
+const parse = issueParser('github', { actions: { blocks: ['blocks'] }});
+const repoToken = core.getInput('repo-token');
+const octokit = github.getOctokit(repoToken);
+
 
 /*
 const issueComments = octokit.issues.listComments({
   owner, repo, issue_number
 });
 */
+async function run() {
+  try {
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = github.context.payload;
+    //console.log(`The event payload: ${JSON.stringify(payload, null, 2)}`);
 
-try {
-  const repoToken = core.getInput('repo-token');
-  const octokit = github.getOctokit(repoToken);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, null, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+    //octokit.paginate(
+    const issues = octokit.issues.listForRepo({
+      state: 'open',
+      owner:  github.context.payload.repository.owner.id,
+      repo:   github.context.payload.repository.full_name,
+    });  
+    console.log(`issues: ${JSON.stringify(issues, null, 2)}`);
+
+    //core.setOutput("time", time);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
 }
 
-
+run();
