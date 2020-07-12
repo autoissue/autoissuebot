5faac21c742dbdb14230152c20df0c4be306ad49
Vat Raghavan <machinshin@gmail.com>
2020-07-12 14:44:50 -0700

    add up blocking issues, sort & push to output

--- dist/index.js
+++ dist/index.js
@@ -12768,19 +12768,27 @@ if ( !repoToken) {
 
 
 
+
+
 function toInt(str) {
   return parseInt(str, 10);
 }
 
 const THIS_ID = toInt(context.payload.issue.number);
 
-function filterBlockers(allIssues, THIS_ID) {
+
+
+
+
+function filterNonBlockers(allIssues, THIS_ID) {
   const validBody = (issue) => { return issue.body !== '' } 
   const notThisIssue = (issue) => toInt(issue.number) !== THIS_ID;;
   const allowedCollaboratorTypes = (issue) => ['COLLABORATOR', 'CONTRIBUTOR', 'MEMBER', 'OWNER' ].includes(issue.author_association);
 
   const onlyBlockers = (issue) => {
     const pBody = parse(issue.body.trim().toLowerCase());
+    console.log(`issue: ${JSON.stringify(issue, null, 2)}`);
+    console.log(`pBody: ${JSON.stringify(pBody, null, 2)}`);
     const blockedIssueNum = toInt(pBody.actions.blocks[0].issue);
     return pBody.actions.blocks && blockedIssueNum  === THIS_ID;
   };
@@ -12789,9 +12797,13 @@ function filterBlockers(allIssues, THIS_ID) {
   return allIssues.filter(validBody).filter(notThisIssue).filter(allowedCollaboratorTypes).filter(onlyBlockers);
 }
 
+
+
+
+
 function postComment(blockers) {
     const blocker_str = blockers.map((blocker) => { return `#${blocker.number}` }).join(' ');
-    const commentBody =`This issue cannot be closed at this time, it is dependent on the following issue(s): ${blocker_str}`;
+    const comment =`This issue cannot be closed at this time, it is dependent on the following issue(s): ${blocker_str}`;
 
     octokit.issues.update({
       owner:  context.repo.owner,
@@ -12803,9 +12815,9 @@ function postComment(blockers) {
       owner:  context.repo.owner,
       repo:   context.repo.repo,
       issue_number:  THIS_ID,
-      body: commentBody,
+      body: comment,
     });
-  return commentBody;
+  return comment;
 }
 
 
@@ -12813,38 +12825,42 @@ function postComment(blockers) {
 async function run() {
   try {
     console.log(`THIS_ID: ${THIS_ID}`);
-    // const all = await getAllIssues();
-    // const sorted = all.sort((l, r) => l.number - r.number );
 
-    const parameters = {
-      state: 'open',
-      owner:  context.repo.owner,
-      repo:   context.repo.repo,
-      //default: per_page: 30,
-      per_page: 1, //for debug only
+    const getNextPage = () => {
+      const parameters = {
+        state: 'open',
+        owner:  context.repo.owner,
+        repo:   context.repo.repo,
+        //default: per_page: 30,
+        per_page: 1, //for debug only
+      };
+      return octokit.paginate.iterator(octokit.issues.listForRepo, parameters);
     };
-    var blockers = [];
-    for await (const response of octokit.paginate.iterator(octokit.issues.listForRepo, parameters)) {
-      // do whatever you want with each response, break out of the loop, etc.
-      //response.data.issues ? 
-      console.log(`listForRepo response: ${JSON.stringify(response, null, 2)}`);
+
+    var allBlockers = [];
+    for await (const response of getNextPage()) {
+      // console.log(`listForRepo response: ${JSON.stringify(response, null, 2)}`);
       //TODO:: filters & add to blockers
+      const issuesThisResp  = response.data;
+      const blockersThisResp = filterNonBlockers(issuesThisResp); 
+      allBlockers.concat(blockersThisResp); 
       //console.log(`allIssues: ${JSON.stringify(allIssues, null, 2)}`);
-      //const blockers = filterBlockers(sorted, THIS_ID);
-      //console.log(`blockers: ${JSON.stringify(blockers, null, 2)}`);
     }
 
-    if(blockers.length == 0) {
+    if(allBlockers.length == 0) {
       core.setOutput('blocking_issues', 'No blocking issues, this issue is now permanently closed');
+      process.exit(0);
       return;
     }
-    
-    const commentBody = postComment(blockers);
-    core.setOutput('blocking_issues', commentBody);
+    allBlockers.sort((l, r) => l.number - r.number );
+    console.log(`all blockers: ${JSON.stringify(allBlockers, null, 2)}`);
+    const comment = postComment(allBlockers);
+    core.setOutput('blocking_issues', comment);
 
   } catch (error) {
     core.setFailed(error.message);
     console.log(`error: ${JSON.stringify(error, null, 2)}`);
+    process.exit(1);
   }
 }
 
